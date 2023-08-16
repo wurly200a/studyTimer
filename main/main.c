76baecf914a172329a287d53c387d524d896bd3b
Wurly <48784425+wurly200a@users.noreply.github.com>
2023-08-16 22:07:37 +0900

    lcdgfx/examples/demos/ssd1331_demo merged

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
@@ -10,9 +11,29 @@
 static const char *TAG = "study_timer_main";
 
 extern void wps_main(void);
+extern void setup(void);
+extern void loop(void);
+
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+void main_task(void *args)
+{
+    setup();
+    for(;;) {
+        loop();
+#ifdef SDL_EMULATION
+        sdl_read_analog(0);
+#endif
+    }
+}
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "APP_MAIN_START");
+    xTaskCreatePinnedToCore(main_task, "mainTask", 8192, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
     wps_main();
 }
